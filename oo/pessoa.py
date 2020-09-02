a11287357c694dbd58a52e2cce6cf890904a198f
theerickcs <e3.cs@live.com>
2020-09-02 19:36:28 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,13 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome='Erick'):
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    erick = Pessoa(nome='Erick')
+    anelito = Pessoa(erick, nome='Anelito')
+    for listagem in anelito.filhos:
+        print(listagem.nome)
